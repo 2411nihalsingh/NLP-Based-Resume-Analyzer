7f251c12b4a8f7b78abc8bf29232ac63a7dfbb40
2411nihalsingh <166745563+2411nihalsingh@users.noreply.github.com>
2025-05-24 21:05:20 +0000

    app.py updated for admin panel

--- app.py
+++ app.py
@@ -3,28 +3,22 @@ from sections import grammar_checker, homepage,ats_check,jobs,admin_panel
 
 st.set_page_config(page_title="Resume Analyzer", layout="wide",page_icon="🤖")
 
-admin_button = st.sidebar.button("Admin Login")
+st.sidebar.title("Select Feature")
+app_mode = st.sidebar.selectbox("Go to", ["Homepage", "ATS Score Check", "Grammar Checker", "Find Jobs","Admin Panel"])
 
-if not admin_button:
-     
-    st.sidebar.title("Navigation")
-    app_mode = st.sidebar.selectbox("Go to", ["Homepage", "ATS Score Check", "Grammar Checker", "Find Jobs"])
+# Page router
+if app_mode == "Homepage":
+    homepage.run()
 
-    # Page router
-    if app_mode == "Homepage":
-        homepage.run()
+elif app_mode == "ATS Score Check":
+    ats_check.run()
 
-    elif app_mode == "ATS Score Check":
-        ats_check.run()
+elif app_mode == "Grammar Checker":
+    grammar_checker.run()
 
-    elif app_mode == "Grammar Checker":
-        grammar_checker.run()
+elif app_mode == "Find Jobs":
+    jobs.run()
 
-    elif app_mode == "Find Jobs":
-        jobs.run()
+elif app_mode == "Admin Panel":
+    admin_panel.run()
     
-else:
-   return_button = st.sidebar.button("return")
-   if(return_button):
-       homepage.run()
-   admin_panel.run()
